e82234504178e1defb6382cc7fd79ffb6118a0a7
Lisis Ruschel <laruschel@proton.me>
2024-03-25 15:59:39 +0100

    Diagram gemacht.

---
+++ src/main/Quiz.kt
@@ -0,0 +1,23 @@
+package main
+
+class Quiz(
+    listOfPlayers: MutableList<Player>,
+    listOfQuestions: MutableList<Question>,
+    roundCount: Int,
+    endCondition: Boolean,
+    numOfPlayers: Int
+) {
+    fun startGame() {
+
+    }
+
+    fun generateQuestion() {
+
+    }
+    fun defineWinner(){
+
+    }
+    fun endGame(){
+
+    }
+}
