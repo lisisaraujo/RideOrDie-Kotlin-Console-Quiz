01075d904f3a38f9941de736c86363b8c9027dac
Lisis Ruschel <laruschel@proton.me>
2024-04-02 14:06:12 +0200

    fixed joker100

--- src/main/Jokers/Joker100.kt
+++ src/main/Jokers/Joker100.kt
@@ -1,13 +1,22 @@
 package main.Jokers
 
 import main.Players.Player
+import main.Questions.MultipleChoiceQuestion
 import main.Questions.Question
 
 class Joker100(player: Player) : Joker(type = "Joker100", player) {
 
+    override fun playJoker(question: MultipleChoiceQuestion): MultipleChoiceQuestion {
+        super.playJoker(question)
+        println("The the correct answer is: ")
+        println(question.possibleAnswers[question.correctAnswer - 1])
+        return question
+    }
+
     override fun playJoker(question: Question): Question {
         super.playJoker(question)
-        println("The the correct answer is: ${question.correctAnswer + 1}. ${question.possibleAnswers[question.correctAnswer]}")
+        println("The the correct answer is: ")
+        println(question.possibleAnswers[question.correctAnswer - 1])
         return question
     }
 }
